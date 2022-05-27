d1af9842c620a4057d07fbf78235cb99028782e5
frank <tahe.nft@gmail.com>
2022-05-27 01:54:07 -0400

    add rinkeby testnet config and dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,15 @@ require("@nomiclabs/hardhat-waffle");
 require('@nomiclabs/hardhat-ethers');
 require('hardhat-gas-reporter');
 
+
+// const path = require('path')
+// require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+require('dotenv').config();
+
+const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY.toString();
+// https://docs.palm.io/HowTo/Deploy-using-Hardhat/
+// dotenv.config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -24,6 +33,15 @@ const settings = {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: ALCHEMY_API_URL,
+      accounts: ["0x"+RINKEBY_PRIVATE_KEY],
+    }
+  },
   solidity: {
     compilers: [
       { version: "0.8.9", settings },
@@ -36,6 +54,36 @@ module.exports = {
   },
 
 };
+
+// module.exports = {
+//   defaultNetwork: "rinkeby",
+//   networks: {
+//     hardhat: {
+//     },
+//     rinkeby: {
+//       url: "https://eth-rinkeby.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+//       accounts: [privateKey1, privateKey2, ...]
+//     }
+//   },
+//   solidity: {
+//     version: "0.5.15",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200
+//       }
+//     }
+//   },
+//   paths: {
+//     sources: "./contracts",
+//     tests: "./test",
+//     cache: "./cache",
+//     artifacts: "./artifacts"
+//   },
+//   mocha: {
+//     timeout: 40000
+//   }
+// }
 
 
 // module.exports = {
