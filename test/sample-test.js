90901462968b6d52dd51090148bd2acac7134a49
frank <tahe.nft@gmail.com>
2022-05-29 11:46:43 -0400

    update test script

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,3 +1,5 @@
+// learn examples from https://github.com/tsubasakong/nft-erc721-collection/blob/main/smart-contract/test/index.ts
+
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const { utils }  = require("ethers")
@@ -21,8 +23,8 @@ describe("Adidas", function () {
     // expect(await greeter.greet()).to.equal("Hola, mundo!");
     [owner, address1] = await ethers.getSigners();
 
-    const Adidas = await hre.ethers.getContractFactory("AdidasOriginals");
-    adidas = await Adidas.deploy("test","test","https://google.com/");
+    const Adidas = await hre.ethers.getContractFactory("Shop3Collection");
+    adidas = await Adidas.deploy("test","test","https://google.com/", "10000000000000000", 0); // unlimited mint
   
     await adidas.deployed();
   
@@ -36,57 +38,37 @@ describe("Adidas", function () {
   });
   // https://dev.to/jacobedawson/import-test-a-popular-nft-smart-contract-with-hardhat-ethers-12i5
   it ("should mint an token", async () => {
-    
-    const mintPrice = await adidas.mintPrice();
+    let min_amount = 10;
+    // use BigNumber to prevent overflow or underflow https://docs.ethers.io/v5/troubleshooting/errors/
+    const mintPrice =  ethers.BigNumber.from(String(await adidas.mintPrice()));
     console.log("mint price",mintPrice);
     expect(
-      await adidas.purchase(1, {
-        value: mintPrice,
+      await adidas.purchase(min_amount, {
+        value: mintPrice.mul(min_amount),
       })
     )
     .to.emit(adidas, "Purchased") //Purchased(0, msg.sender, amount);
-    .withArgs(0, owner,1);
+    .withArgs(0, owner,min_amount);
 
     console.log("token uri", await adidas.uri(0))
   })
+
+  // insufficient purchase value
+  it("insufficnet purchase value", async () => {
+    let min_amount = 1;
+
+    const mintPrice =  ethers.BigNumber.from(String(await adidas.mintPrice()));
+    const send_value = mintPrice.mul(min_amount).div(2);
+    // await before "expet", not inside the transaction call "purchase"
+    await expect(
+      adidas.purchase(min_amount, {
+        value: send_value,
+      })
+    ).to.be.revertedWith("Purchase: Incorrect payment");
+  }
+
+
+  )
+  
 });
 
-// describe("Bored Ape", () => {
-//   let boredApeContract: Contract;
-//   let owner: SignerWithAddress;
-//   let address1: SignerWithAddress;
-
-//   beforeEach(async () => {
-//     const BoredApeFactory = await ethers.getContractFactory(
-//       "BoredApeYachtClub"
-//     );
-//     [owner, address1] = await ethers.getSigners();
-//     boredApeContract = await BoredApeFactory.deploy(
-//       "Bored Ape Yacht Club",
-//       "BAYC",
-//       10000,
-//       1
-//     );
-//   });
-
-//   it("Should initialize the Bored Ape contract", async () => {
-//     expect(await boredApeContract.MAX_APES()).to.equal(10000);
-//   });
-
-//   it("Should set the right owner", async () => {
-//     expect(await boredApeContract.owner()).to.equal(await owner.address);
-//   });
-
-//   it("Should mint an ape", async () => {
-//     await boredApeContract.flipSaleState();
-//     const apePrice = await boredApeContract.apePrice();
-//     const tokenId = await boredApeContract.totalSupply();
-//     expect(
-//       await boredApeContract.mintApe(1, {
-//         value: apePrice,
-//       })
-//     )
-//       .to.emit(boredApeContract, "Transfer")
-//       .withArgs(ethers.constants.AddressZero, owner.address, tokenId);
-//   });
-// });
